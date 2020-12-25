d87615fedce96c0e064005269893bb30e8ecd90b
East Agile <eastagile@Easts-iMac.local>
2020-12-25 11:15:14 +0700

    [f] Integrate coveralls
    [#176269628] (https://www.pivotaltracker.com/story/show/176269628)

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
-  docker-compose run web python -m pytest ipno/
+  docker-compose run web python -m pytest --cov-report term  --cov=ipno ipno/
 else
   docker-compose run web python -m pytest $1
 fi
