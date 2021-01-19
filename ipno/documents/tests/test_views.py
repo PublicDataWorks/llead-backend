5d006ce12313b72085b89d5110df920fa79618fa
ThaoT + HuyN <thao.tran+huy.d.nguyen@eastagile.com>
2021-01-19 15:18:47 +0700

    [f] 810a - Login API
    [#176434813] (https://www.pivotaltracker.com/story/show/176434813)

--- ipno/documents/tests/test_views.py
+++ ipno/documents/tests/test_views.py
@@ -1,19 +1,25 @@
 from django.urls import reverse
 
-from rest_framework.test import APITestCase
 from rest_framework import status
 
 from documents.factories import DocumentFactory
+from test_utils.auth_api_test_case import AuthAPITestCase
 
 
-class DocumentViewSetTestCase(APITestCase):
-    def test_retrieve(self):
+class DocumentViewSetTestCase(AuthAPITestCase):
+    def test_retrieve_success(self):
         document = DocumentFactory()
 
-        response = self.client.get(reverse('api:documents-detail', kwargs={'pk': document.id}))
+        response = self.auth_client.get(reverse('api:documents-detail', kwargs={'pk': document.id}))
         assert response.status_code == status.HTTP_200_OK
 
         assert response.data == {
             'id': document.id,
             'title': document.title,
         }
+
+    def test_retrieve_unauthorized(self):
+        document = DocumentFactory()
+
+        response = self.client.get(reverse('api:documents-detail', kwargs={'pk': document.id}))
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
--- ipno/documents/views.py
+++ ipno/documents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from documents.models import Document
@@ -8,6 +9,8 @@ from documents.serializers import DocumentSerializer
 
 
 class DocumentViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
     def retrieve(self, request, pk):
         queryset = Document.objects.all()
         document = get_object_or_404(queryset, id=pk)
