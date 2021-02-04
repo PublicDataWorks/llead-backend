e749168eb9450eef1dc1608bbe992f13c6716cfc
East Agile <open-source@eastagile.com>
2021-02-04 12:54:15 +0700

    [f] Add OfficerHistoryAdmin

--- ipno/officers/admin.py
+++ ipno/officers/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from officers.models import Officer
+from officers.models import Officer, OfficerHistory
 
 
 class OfficerAdmin(ModelAdmin):
     list_display = ('id', 'uid', 'last_name', 'first_name', 'created_at', 'updated_at')
 
 
+class OfficerHistoryAdmin(ModelAdmin):
+    list_display = ('id', 'officer', 'department', 'badge_no', 'rank_code', 'start_date', 'end_date')
+
+
 admin.site.register(Officer, OfficerAdmin)
+admin.site.register(OfficerHistory, OfficerHistoryAdmin)
