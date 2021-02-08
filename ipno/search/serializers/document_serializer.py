5749b8f2b2ce981fd998a3d904e8057270936619
East Agile <open-source@eastagile.com>
2021-02-08 17:09:20 +0700

    [f] Add highlight for documents search text content

--- ipno/search/serializers/document_serializer.py
+++ ipno/search/serializers/document_serializer.py
@@ -5,3 +5,10 @@ from shared.serializers import BaseDocumentSerializer
 
 class DocumentSerializer(BaseDocumentSerializer):
     text_content = serializers.CharField()
+    text_content_highlight = serializers.SerializerMethodField()
+
+    def get_text_content_highlight(self, obj):
+        try:
+            return obj.es_doc.meta.highlight.text_content[0]
+        except AttributeError:
+            return
