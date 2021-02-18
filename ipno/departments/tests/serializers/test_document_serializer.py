4ec8ec8fb0cb6f3c2308526fb2c9e4e17a0d4d52
Thao Tran <thao.tran@eastagile.com>
2021-02-18 17:35:43 +0700

    [f] 167a - Department documents API
    [#176610201] (https://www.pivotaltracker.com/story/show/176610201)

---
+++ ipno/departments/tests/serializers/test_document_serializer.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+
+from departments.serializers import DocumentSerializer
+from documents.factories import DocumentFactory
+
+
+class DocumentSerializerTestCase(TestCase):
+    def test_data(self):
+        document = DocumentFactory()
+
+        result = DocumentSerializer(document).data
+
+        assert result == {
+            'id': document.id,
+            'document_type': document.document_type,
+            'title': document.title,
+            'url': document.url,
+            'incident_date': document.incident_date,
+            'preview_image_url': document.preview_image_url,
+            'pages_count': document.pages_count,
+        }
