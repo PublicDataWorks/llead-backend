589fade7d967c650629a31338dac72365214bf96
East Agile <open-source@eastagile.com>
2021-02-24 17:12:20 +0700

    [f] 276 - Department page - data period info
    [#176610276] (https://www.pivotaltracker.com/story/show/176610276)

--- ipno/departments/serializers/department_details_serializer.py
+++ ipno/departments/serializers/department_details_serializer.py
@@ -27,6 +27,7 @@ class DepartmentDetailsSerializer(serializers.Serializer):
     complaints_count = serializers.SerializerMethodField()
     documents_count = serializers.SerializerMethodField()
     wrgl_files = serializers.SerializerMethodField()
+    data_period = serializers.SerializerMethodField()
 
     @staticmethod
     def filter_by_department(klass, department_id):
@@ -55,3 +56,31 @@ class DepartmentDetailsSerializer(serializers.Serializer):
 
     def get_wrgl_files(self, obj):
         return WrglFileSerializer(obj.wrglfile_set.order_by('position'), many=True).data
+
+    def get_data_period(self, obj):
+        years = obj.officerhistory_set.filter(
+            start_date__isnull=False,
+            end_date__isnull=False
+        ).order_by('start_date__year').values_list('start_date__year', 'end_date__year')
+
+        data_period = []
+        current_start_year = None
+        current_end_year = None
+        for start_year, end_year in years:
+            if current_start_year is None:
+                current_start_year = start_year
+                current_end_year = end_year
+            elif start_year <= current_end_year:
+                current_end_year = end_year
+            else:
+                data_period.append([current_start_year, current_end_year])
+                current_start_year = start_year
+                current_end_year = end_year
+
+        if current_start_year:
+            data_period.append([current_start_year, current_end_year])
+
+        return [
+            f'{start_year}{f"-{end_year}" if start_year != end_year else ""}'
+            for start_year, end_year in data_period
+        ]
