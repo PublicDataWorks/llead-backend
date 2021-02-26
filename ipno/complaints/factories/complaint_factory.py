ee39b1339810299e3b0d9e817220ef7356034143
East Agile <open-source@eastagile.com>
2021-02-26 13:16:39 +0700

    [f] Update data structure

--- ipno/complaints/factories/complaint_factory.py
+++ ipno/complaints/factories/complaint_factory.py
@@ -1,5 +1,3 @@
-import random
-
 import factory
 from faker import Faker
 
@@ -12,5 +10,4 @@ class ComplaintFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Complaint
 
-    uid = factory.LazyFunction(lambda: str(random.randint(100000, 999999)))
     incident_date = factory.LazyFunction(lambda: fake.date())
--- ipno/officers/models/officer.py
+++ ipno/officers/models/officer.py
@@ -4,15 +4,15 @@ from utils.models import TimeStampsModel
 
 
 class Officer(TimeStampsModel):
-    uid = models.CharField(max_length=255)
+    uid = models.CharField(max_length=255, unique=True, db_index=True)
     last_name = models.CharField(max_length=255, null=True, blank=True)
     middle_name = models.CharField(max_length=255, null=True, blank=True)
     middle_initial = models.CharField(max_length=255, null=True, blank=True)
     first_name = models.CharField(max_length=255, null=True, blank=True)
     employee_id = models.CharField(max_length=255, null=True, blank=True)
-    birth_year = models.IntegerField()
-    birth_month = models.IntegerField()
-    birth_day = models.IntegerField()
+    birth_year = models.IntegerField(null=True)
+    birth_month = models.IntegerField(null=True)
+    birth_day = models.IntegerField(null=True)
 
     departments = models.ManyToManyField('departments.Department', through='officers.OfficerHistory')
 
