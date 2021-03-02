b9e2f3f2a00b41556d5461e98ca4cba0ed17b989
East Agile <open-source@eastagile.com>
2021-03-02 12:16:31 +0700

    874 - In django admin, display id-officer_name and id-department_name for officer & department reference
    [#177155874] (https://www.pivotaltracker.com/story/show/177155874)

--- ipno/officers/tests/models/test_officer.py
+++ ipno/officers/tests/models/test_officer.py
@@ -7,3 +7,7 @@ class OfficerTestCase(TestCase):
     def test_name(selfs):
         officer = OfficerFactory(first_name='David', last_name='Jonesworth')
         assert officer.name == 'David Jonesworth'
+
+    def test_str(self):
+        officer = OfficerFactory()
+        assert str(officer) == f"{officer.id} - {officer.name}"
