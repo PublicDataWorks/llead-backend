e17856d37bdd48c5d61de0facc0c4e3f8e2cebfb
ThaoT + HuyN <thao.tran+huy.d.nguyen@eastagile.com>
2021-03-04 11:38:57 +0700

    [b] 583 - In Django for a document, the Incident date should not be a required field

--- ipno/documents/models/document.py
+++ ipno/documents/models/document.py
@@ -8,7 +8,7 @@ class Document(TimeStampsModel):
     document_type = models.CharField(max_length=255, null=True, blank=True)
     url = models.CharField(max_length=255, null=True, blank=True)
     preview_image_url = models.CharField(max_length=255, null=True, blank=True)
-    incident_date = models.DateField(null=True)
+    incident_date = models.DateField(null=True, blank=True)
     pages_count = models.IntegerField(null=True, blank=True)
     text_content = models.TextField(blank=True)
 
