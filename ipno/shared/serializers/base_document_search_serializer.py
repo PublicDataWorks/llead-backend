c5780a01471ebbc51880d2e1fd651792f901e021
East Agile <open-source@eastagile.com>
2021-03-12 11:19:07 +0700

    [f] 316 - Update department documents UI
    [#177297316] (https://www.pivotaltracker.com/story/show/177297316)

--- ipno/shared/serializers/base_document_search_serializer.py
+++ ipno/shared/serializers/base_document_search_serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from search.constants import TEXT_CONTENT_LIMIT
+from shared.constants import TEXT_CONTENT_LIMIT
 
 
 class BaseDocumentSearchSerializer(serializers.Serializer):
