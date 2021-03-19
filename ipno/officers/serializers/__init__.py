95bee275b2da6b07fab2aefdae84fcf42873cfc5
East Agile <open-source@eastagile.com>
2021-03-19 15:28:17 +0700

    [f] 351 - Officer page - Documents section
    [#176610351] (https://www.pivotaltracker.com/story/show/176610351)

--- ipno/officers/serializers/__init__.py
+++ ipno/officers/serializers/__init__.py
@@ -1,5 +1,7 @@
 from .officer_details_serializer import OfficerDetailsSerializer
+from .document_serializer import DocumentSerializer
 
 __all__ = [
     'OfficerDetailsSerializer',
+    'DocumentSerializer'
 ]
--- ipno/officers/views.py
+++ ipno/officers/views.py
@@ -4,11 +4,13 @@ from django.db.models import Prefetch
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from officers.models import Officer, OfficerHistory
 from shared.serializers import OfficerSerializer
 from officers.serializers import OfficerDetailsSerializer
 from officers.constants import OFFICERS_LIMIT
+from officers.serializers import DocumentSerializer
 
 
 class OfficersViewSet(viewsets.ViewSet):
@@ -31,4 +33,12 @@ class OfficersViewSet(viewsets.ViewSet):
         officer = get_object_or_404(Officer, id=pk)
         serializer = OfficerDetailsSerializer(officer)
 
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='documents')
+    def documents(self, request, pk):
+        officer = get_object_or_404(Officer, id=pk)
+        documents = officer.document_set.prefetch_departments().order_by('-incident_date')
+
+        serializer = DocumentSerializer(documents, many=True)
         return Response(serializer.data)
