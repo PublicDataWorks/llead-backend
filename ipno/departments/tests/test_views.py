4f1fa4da1220fce483cb3273207b80663f890ffa
East Agile <open-source@eastagile.com>
2021-04-29 12:48:58 +0700

    Revert "[f] 537 - Officer timeline - Only show complaints with allegation_finding is sustained"
    This reverts commit 3054777b45834610d0009aa8d508159fb874eff0.

--- ipno/departments/tests/test_views.py
+++ ipno/departments/tests/test_views.py
@@ -133,8 +133,7 @@ class DepartmentsViewSetTestCase(AuthAPITestCase):
         for document in documents:
             document.departments.add(department)
 
-        complaints = ComplaintFactory.create_batch(2, complaint_uid='complaint_uid1') + \
-            ComplaintFactory.create_batch(3, complaint_uid='complaint_uid2')
+        complaints = ComplaintFactory.create_batch(2)
         ComplaintFactory()
         for complaint in complaints:
             complaint.departments.add(department)
