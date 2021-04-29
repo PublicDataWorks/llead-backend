f3f48d007b41ab651ca024757c8ffec39e8df875
East Agile <open-source@eastagile.com>
2021-04-29 12:48:58 +0700

    [f] 537 - Officer timeline - Only show complaints with allegation_finding is sustained
    [#177959537] (https://www.pivotaltracker.com/story/show/177959537)

--- ipno/departments/tests/test_views.py
+++ ipno/departments/tests/test_views.py
@@ -133,7 +133,8 @@ class DepartmentsViewSetTestCase(AuthAPITestCase):
         for document in documents:
             document.departments.add(department)
 
-        complaints = ComplaintFactory.create_batch(2)
+        complaints = ComplaintFactory.create_batch(2, complaint_uid='complaint_uid1') + \
+            ComplaintFactory.create_batch(3, complaint_uid='complaint_uid2')
         ComplaintFactory()
         for complaint in complaints:
             complaint.departments.add(department)
