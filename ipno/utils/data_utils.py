fbed59df707fcb5196f4ddf886aee8244b5e017c
ThaoT + HuyN <thao.tran+huy.d.nguyen@eastagile.com>
2021-04-29 16:48:59 +0700

    [f] 238 - Officer timeline - Handle rank changes & salary changes row with missing data
    [#177959238] (https://www.pivotaltracker.com/story/show/177959238)
    [#177959260] (https://www.pivotaltracker.com/story/show/177959260)

--- ipno/utils/data_utils.py
+++ ipno/utils/data_utils.py
@@ -13,3 +13,17 @@ def data_period(years):
         f'{item[0]}{f"-{item[-1]}" if len(item) > 1 else ""}'
         for item in results
     ]
+
+
+def sort_items(items, attrs):
+    def get_sort_key(item, item_attrs):
+        results = []
+        for attr in item_attrs:
+            value = getattr(item, attr)
+            results.append((value is None, str(value)))
+        return results
+
+    return sorted(
+        items,
+        key=lambda item: get_sort_key(item, attrs)
+    )
