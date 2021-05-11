919a05396f8b85789d77c896809c3a3901116da7
East Agile <open-source@eastagile.com>
2021-05-11 14:49:19 +0700

    [f] Footer editable with hyperlink
    [#177900927] (https://www.pivotaltracker.com/story/show/177900927)
    [#177089600] (https://www.pivotaltracker.com/story/show/177089600)

--- ipno/app_config/admin/app_text_content_admin.py
+++ ipno/app_config/admin/app_text_content_admin.py
@@ -1,4 +1,8 @@
+from django.db import models
 from django.contrib import admin
+
+from martor.widgets import AdminMartorWidget
+
 from app_config.models import AppTextContent
 
 
@@ -8,6 +12,9 @@ class AppTextContentAdmin(admin.ModelAdmin):
         'name', 'value', 'description'
     )
     readonly_fields = ['name', 'description']
+    formfield_overrides = {
+        models.TextField: {'widget': AdminMartorWidget},
+    }
 
     def has_delete_permission(self, request, obj=None):  # pragma: no cover
         return False
--- ipno/app_config/constants.py
+++ ipno/app_config/constants.py
@@ -9,8 +9,14 @@ APP_CONFIG = [
 APP_TEXT_CONTENTS = [
     {
         'name': 'FRONT_PAGE_SUMMARY',
-        'value': 'We are building a database of <b>Louisiana</b> police officers, departments, and documents.',
+        'value': 'We are building a database of **Louisiana** police officers, departments, and documents.',
         'description': 'The summary text display in frontpage.'
+    },
+    {
+        'name': 'FOOTER_TEXT',
+        'value': '[**Innocence Project New Orleans**](https://ip-no.org) in collaboration with [**Public Data Works**]'
+                 '(https://publicdata.works)',
+        'description': 'The footer text display in every page.'
     }
 ]
 
