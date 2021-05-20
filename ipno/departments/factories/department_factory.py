824bf1e619a2de1e510f850557fe184fed65ed53
East Agile <open-source@eastagile.com>
2021-05-20 17:38:24 +0700

    [t] Make sure department factory name is unique

--- ipno/departments/factories/department_factory.py
+++ ipno/departments/factories/department_factory.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.utils.text import slugify
 
 import factory
@@ -12,5 +14,5 @@ class DepartmentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Department
 
-    name = factory.LazyFunction(lambda: f"{fake.city()} PD")
+    name = factory.LazyFunction(lambda: f"{fake.city()} {datetime.utcnow().strftime('%H:%M:%S')} PD")
     slug = factory.LazyAttribute(lambda d: slugify(d.name))
