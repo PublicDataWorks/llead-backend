f0969887dfbcf4a28906bf837f9a26c90de42844
HuyN + TuT <huy.d.nguyen+tu.tran@eastagile.com>
2021-08-31 09:06:52 +0700

    [f] 387a - Add news article model
    [#179278895](https://www.pivotaltracker.com/story/show/179278895)

---
+++ ipno/news_articles/models/news_article.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from utils.models import TimeStampsModel
+
+
+class NewsArticle(TimeStampsModel):
+    name = models.CharField(max_length=255)
+    guid = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField()
+    author = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    preview_image_url = models.CharField(max_length=255, blank=True, null=True)
+
+    officers = models.ManyToManyField('officers.Officer', blank=True, related_name='news_articles')
