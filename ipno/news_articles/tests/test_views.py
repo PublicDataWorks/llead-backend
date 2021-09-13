297b6e3aa5e38a0d445a2faf07e55309978c06a0
HuyN + TuT <huy.d.nguyen+tu.tran@eastagile.com>
2021-09-13 09:15:47 +0700

    [f] 206a - Get latest news articles API
    [#179488086](https://www.pivotaltracker.com/story/show/179488086)

---
+++ ipno/news_articles/tests/test_views.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.urls import reverse
+
+from rest_framework import status
+
+from news_articles.factories import NewsArticleFactory
+from news_articles.models import NewsArticleSource
+from test_utils.auth_api_test_case import AuthAPITestCase
+
+
+class NewsArticlesViewSetTestCase(AuthAPITestCase):
+    def test_list_success(self):
+        source = NewsArticleSource()
+        news_article_1 = NewsArticleFactory(source_name=source.source_name)
+        news_article_2 = NewsArticleFactory(
+            source_name=source.source_name,
+            published_date=news_article_1.published_date + datetime.timedelta(days=1)
+        )
+
+        response = self.auth_client.get(reverse('api:news-articles-list'))
+        assert response.status_code == status.HTTP_200_OK
+
+        expected_data = [
+            {
+                'id': news_article_1.id,
+                'date': str(news_article_1.published_date),
+                'title': news_article_1.title,
+                'url': news_article_1.url,
+                'source_name': source.custom_matching_name,
+            },
+            {
+                'id': news_article_2.id,
+                'date': str(news_article_2.published_date),
+                'title': news_article_2.title,
+                'url': news_article_2.url,
+                'source_name': source.custom_matching_name,
+            },
+        ]
+
+        assert response.data == expected_data
+
+    def test_list_unauthorized(self):
+        response = self.client.get(reverse('api:news-articles-list'))
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
---
+++ ipno/news_articles/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from news_articles.models import NewsArticle
+from shared.serializers import NewsArticleSerializer
+from news_articles.constants import NEWS_ARTICLES_LIMIT
+
+
+class NewsArticlesViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        news_articles = NewsArticle.objects.order_by(
+            'published_date',
+        )[:NEWS_ARTICLES_LIMIT]
+
+        serializer = NewsArticleSerializer(news_articles, many=True)
+        return Response(serializer.data)
