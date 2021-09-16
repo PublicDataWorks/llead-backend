07adf569e7ff30c7ef1370688e66ba318c0515eb
HuyN + TuT <huy.d.nguyen+tu.tran@eastagile.com>
2021-09-16 15:24:32 +0700

    [c] Improve admin UX on changing order

--- ipno/app_config/admin/front_page_order_admin.py
+++ ipno/app_config/admin/front_page_order_admin.py
@@ -1,7 +1,15 @@
-from adminsortable.admin import SortableAdmin
+from adminsortable2.admin import SortableAdminMixin
 from django.contrib import admin
 
 from app_config.models import FrontPageOrder
 
 
-admin.site.register(FrontPageOrder, SortableAdmin)
+@admin.register(FrontPageOrder)
+class MyModelAdmin(SortableAdminMixin, admin.ModelAdmin):
+    readonly_fields = ('section', 'order')
+
+    def has_add_permission(self, request, obj=None):
+        return False  # pragma: no cover
+
+    def has_delete_permission(self, request, obj=None):
+        return False  # pragma: no cover
