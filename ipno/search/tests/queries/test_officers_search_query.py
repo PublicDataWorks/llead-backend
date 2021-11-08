9924cb8d0d5632b96be6e813964fae21f0946f0d
HuyN + TuT <huy.d.nguyen+tu.tran@eastagile.com>
2021-11-08 16:53:31 +0700

    [f] 451d - Update officer-person search
    [#180190423](https://www.pivotaltracker.com/story/show/180190423)

--- ipno/search/tests/queries/test_officers_search_query.py
+++ ipno/search/tests/queries/test_officers_search_query.py
@@ -2,15 +2,27 @@ from django.test import TestCase
 
 from officers.factories import OfficerFactory, EventFactory
 from departments.factories import DepartmentFactory
+from people.factories import PersonFactory
 from search.queries.officers_search_query import OfficersSearchQuery
 from utils.search_index import rebuild_search_index
 
 
 class OfficersSearchQueryTestCase(TestCase):
     def test_query(self):
-        OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        officer = OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        person = PersonFactory(canonical_officer=officer)
+        person.officers.add(officer)
+        person.save()
+
         officer_1 = OfficerFactory(first_name='David', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+
         officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis')
+        person_2 = PersonFactory(canonical_officer=officer_2)
+        person_2.officers.add(officer_2)
+        person_2.save()
 
         rebuild_search_index()
 
@@ -21,7 +33,14 @@ class OfficersSearchQueryTestCase(TestCase):
 
     def test_query_with_matched_by_badges(self):
         officer_1 = OfficerFactory(first_name='David', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+
         officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis')
+        person_2 = PersonFactory(canonical_officer=officer_2)
+        person_2.officers.add(officer_2)
+        person_2.save()
 
         EventFactory(officer=officer_1, badge_no='12435')
         EventFactory(officer=officer_2, badge_no='45812')
@@ -35,8 +54,19 @@ class OfficersSearchQueryTestCase(TestCase):
 
     def test_query_with_matched_by_department_names(self):
         officer_1 = OfficerFactory(first_name='David', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+
         officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis')
+        person_2 = PersonFactory(canonical_officer=officer_2)
+        person_2.officers.add(officer_2)
+        person_2.save()
+
         officer_3 = OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        person_3 = PersonFactory(canonical_officer=officer_3)
+        person_3.officers.add(officer_3)
+        person_3.save()
 
         department_1 = DepartmentFactory(name='New Orleans PD')
         department_2 = DepartmentFactory(name='Baton Rouge PD')
@@ -51,3 +81,25 @@ class OfficersSearchQueryTestCase(TestCase):
         document_ids = {item['id'] for item in result}
 
         assert document_ids == {officer_1.id, officer_2.id}
+
+    def test_query_with_related_officers(self):
+        officer = OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        person = PersonFactory(canonical_officer=officer)
+        person.officers.add(officer)
+        person.save()
+
+        officer_1 = OfficerFactory(first_name='David', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+
+        officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis')
+        person_1.officers.add(officer_2)
+        person_1.save()
+
+        rebuild_search_index()
+
+        result = OfficersSearchQuery('Davi').search()
+        officer_ids = {item['id'] for item in result}
+
+        assert officer_ids == {officer_1.id}
--- ipno/search/tests/test_views.py
+++ ipno/search/tests/test_views.py
@@ -10,6 +10,7 @@ from documents.factories import DocumentFactory
 from news_articles.factories import NewsArticleFactory, NewsArticleSourceFactory
 from news_articles.factories.matched_sentence_factory import MatchedSentenceFactory
 from officers.factories import EventFactory, OfficerFactory
+from people.factories import PersonFactory
 from test_utils.auth_api_test_case import AuthAPITestCase
 from utils.search_index import rebuild_search_index
 
@@ -22,7 +23,13 @@ class SearchViewSetTestCase(AuthAPITestCase):
 
         OfficerFactory(first_name='Kenneth', last_name='Anderson')
         officer_1 = OfficerFactory(first_name='David keyword', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
         officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis keywords')
+        person_2 = PersonFactory(canonical_officer=officer_2)
+        person_2.officers.add(officer_2)
+        person_2.save()
 
         EventFactory(
             officer=officer_1,
@@ -180,8 +187,15 @@ class SearchViewSetTestCase(AuthAPITestCase):
         department_1 = DepartmentFactory(name='New Orleans keyword PD')
         DepartmentFactory(name='Orleans keywo PD')
 
-        OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        officer = OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        person = PersonFactory(canonical_officer=officer)
+        person.officers.add(officer)
+        person.save()
+
         officer_1 = OfficerFactory(first_name='David keyword', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
         OfficerFactory(first_name='Anthony', last_name='Davis keywords')
 
         EventFactory(
@@ -243,9 +257,20 @@ class SearchViewSetTestCase(AuthAPITestCase):
         department_1 = DepartmentFactory(name='New Orleans keyword PD')
         department_2 = DepartmentFactory(name='Orleans keywo PD')
 
-        OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        officer = OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        person = PersonFactory(canonical_officer=officer)
+        person.officers.add(officer)
+        person.save()
+
         officer_1 = OfficerFactory(first_name='David keyword', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+
         officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis keywords')
+        person_2 = PersonFactory(canonical_officer=officer_2)
+        person_2.officers.add(officer_2)
+        person_2.save()
 
         EventFactory(
             officer=officer_1,
@@ -359,3 +384,162 @@ class SearchViewSetTestCase(AuthAPITestCase):
     def test_list_unauthorized(self):
         response = self.client.get(reverse('api:search-list'), {'q': 'keywo'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_list_success_with_related_officer(self):
+        DepartmentFactory(name='Baton Rouge PD')
+        department_1 = DepartmentFactory(name='New Orleans keyword PD')
+        department_2 = DepartmentFactory(name='Orleans keywo PD')
+
+        OfficerFactory(first_name='Kenneth', last_name='Anderson')
+        officer_1 = OfficerFactory(first_name='David keyword', last_name='Jonesworth')
+        person_1 = PersonFactory(canonical_officer=officer_1)
+        person_1.officers.add(officer_1)
+        person_1.save()
+        officer_2 = OfficerFactory(first_name='Anthony', last_name='Davis keywords')
+        person_1.officers.add(officer_2)
+        person_1.save()
+
+        EventFactory(
+            officer=officer_1,
+            department=department_1,
+            badge_no='12435',
+        )
+
+        DocumentFactory(title='Document title', text_content='Text content')
+        document_1 = DocumentFactory(
+            title='Document keyword1',
+            text_content='Text content 1',
+            incident_date=date(2020, 5, 6)
+        )
+        document_2 = DocumentFactory(title='Document 2', text_content='Text content keywo')
+        document_1.departments.add(department_1)
+
+        source = NewsArticleSourceFactory(source_display_name='Source')
+        news_article_1 = NewsArticleFactory(content='Text content keywo', author='Writer Staff', source=source)
+        news_article_2 = NewsArticleFactory(
+            title='Dummy title',
+            author='text keywo',
+            source=source,
+            published_date=news_article_1.published_date + datetime.timedelta(days=1)
+        )
+        matched_sentence_1 = MatchedSentenceFactory(article=news_article_1)
+        matched_sentence_2 = MatchedSentenceFactory(article=news_article_2)
+        matched_sentence_1.officers.add(officer_1)
+        matched_sentence_2.officers.add(officer_2)
+
+        rebuild_search_index()
+
+        expected_data = {
+            'departments': {
+                'results': [
+                    {
+                        'id': department_1.slug,
+                        'name': department_1.name,
+                        'city': department_1.city,
+                        'parish': department_1.parish,
+                        'location_map_url': department_1.location_map_url,
+                    },
+                    {
+                        'id': department_2.slug,
+                        'name': department_2.name,
+                        'city': department_2.city,
+                        'parish': department_2.parish,
+                        'location_map_url': department_2.location_map_url,
+                    }, ],
+                'count': 2,
+                'next': None,
+                'previous': None,
+            },
+            'officers': {
+                'results': [
+                    {
+                        'id': officer_1.id,
+                        'name': officer_1.name,
+                        'badges': ['12435'],
+                        'department': {
+                            'id': department_1.slug,
+                            'name': department_1.name,
+                        },
+                    },
+                ],
+                'count': 1,
+                'next': None,
+                'previous': None,
+            },
+            'documents': {
+                'results': [
+                    {
+                        'id': document_1.id,
+                        'document_type': document_1.document_type,
+                        'title': document_1.title,
+                        'url': document_1.url,
+                        'incident_date': str(document_1.incident_date),
+                        'preview_image_url': document_1.preview_image_url,
+                        'pages_count': document_1.pages_count,
+                        'text_content': document_1.text_content,
+                        'text_content_highlight': None,
+                        'departments': [
+                            {
+                                'id': department_1.slug,
+                                'name': department_1.name,
+                            },
+                        ],
+                    },
+                    {
+                        'id': document_2.id,
+                        'document_type': document_2.document_type,
+                        'title': document_2.title,
+                        'url': document_2.url,
+                        'incident_date': str(document_2.incident_date),
+                        'preview_image_url': document_2.preview_image_url,
+                        'pages_count': document_2.pages_count,
+                        'text_content': document_2.text_content,
+                        'text_content_highlight': 'Text content <em>keywo</em>',
+                        'departments': [],
+                    },
+                ],
+                'count': 2,
+                'next': None,
+                'previous': None,
+            },
+            'articles': {
+                'results': [
+                    {
+                        'id': news_article_1.id,
+                        'source_name': 'Source',
+                        'title': news_article_1.title,
+                        'url': news_article_1.url,
+                        'date': str(news_article_1.published_date),
+                        'author': news_article_1.author,
+                        'content': news_article_1.content,
+                        'content_highlight': 'Text content <em>keywo</em>',
+                        'author_highlight': None
+                    },
+                    {
+                        'id': news_article_2.id,
+                        'source_name': 'Source',
+                        'title': news_article_2.title,
+                        'url': news_article_2.url,
+                        'date': str(news_article_2.published_date),
+                        'author': news_article_2.author,
+                        'content': news_article_2.content,
+                        'content_highlight': None,
+                        'author_highlight': 'text <em>keywo</em>'
+                    },
+                ],
+                'count': 2,
+                'next': None,
+                'previous': None,
+            },
+
+        }
+
+        response = self.auth_client.get(reverse('api:search-list'), {'q': 'keywo'})
+        assert response.status_code == status.HTTP_200_OK
+
+        data = response.data
+
+        for search_key, items in data.items():
+            data[search_key]['results'] = sorted(items['results'], key=itemgetter('id'))
+
+        assert data == expected_data
