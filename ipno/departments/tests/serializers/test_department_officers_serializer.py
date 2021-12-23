47c7b19b0cb751c6648e26f1731ae91270f92fec
HuyN + BaoD <huy.d.nguyen+bao.dang@eastagile.com>
2021-12-23 17:22:53 +0700

    [f] 369b & 369c - Department featured officers
    [#180645387](https://www.pivotaltracker.com/story/show/180645387)
    [#180645532](https://www.pivotaltracker.com/story/show/180645532)

---
+++ ipno/departments/tests/serializers/test_department_officers_serializer.py
@@ -0,0 +1,82 @@
+from django.test import TestCase
+
+from django.db.models import BooleanField, Count
+from django.db.models.expressions import Value
+
+from departments.factories import DepartmentFactory
+from departments.serializers import DepartmentOfficerSerializer
+from officers.constants import UOF_RECEIVE
+from officers.factories import OfficerFactory, EventFactory
+from officers.models import Officer
+from people.factories import PersonFactory
+from use_of_forces.factories import UseOfForceFactory
+
+
+class DepartmentOfficerSerializerTestCase(TestCase):
+    def test_data(self):
+        officer_1 = OfficerFactory()
+        officer_2 = OfficerFactory()
+        person = PersonFactory(canonical_officer=officer_2, all_complaints_count=120)
+        person.officers.add(officer_1)
+        person.officers.add(officer_2)
+        person.save()
+
+        department = DepartmentFactory()
+
+        use_of_force = UseOfForceFactory(officer=officer_1)
+
+        EventFactory(
+            department=department,
+            officer=officer_1,
+            badge_no="150",
+            year=2019,
+            month=2,
+            day=3,
+        )
+
+        EventFactory(
+            department=department,
+            officer=officer_1,
+            badge_no="250",
+            year=2018,
+            month=8,
+            day=3,
+        )
+
+        EventFactory(
+            department=department,
+            officer=officer_2,
+            badge_no="123",
+            year=2018,
+            month=9,
+            day=3,
+        )
+
+        uof_event = EventFactory(
+            department=department,
+            officer=officer_1,
+            kind=UOF_RECEIVE,
+            year=2018,
+            month=8,
+            day=3,
+        )
+
+        use_of_force.events.add(uof_event)
+
+        serialized_officer = Officer.objects.filter(
+            id=officer_1.id
+        ).annotate(
+            is_starred=Value(True, output_field=BooleanField()),
+            use_of_forces_count=Count('use_of_forces'),
+        ).first()
+
+        result = DepartmentOfficerSerializer(serialized_officer).data
+
+        assert result == {
+                'id': officer_1.id,
+                'name': officer_1.name,
+                'badges': ["150", "250", "123"],
+                'is_starred': True,
+                'complaints_count': officer_1.person.all_complaints_count,
+                'use_of_forces_count': 1,
+            }
--- ipno/departments/views.py
+++ ipno/departments/views.py
@@ -1,5 +1,8 @@
+from itertools import chain
+
 from django.shortcuts import get_object_or_404
-from django.db.models import Count
+from django.db.models import Count, BooleanField, Prefetch
+from django.db.models.expressions import F, Value
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
@@ -8,7 +11,8 @@ from rest_framework.decorators import action
 from rest_framework.pagination import LimitOffsetPagination
 
 from departments.models import Department
-from departments.serializers import WrglFileSerializer
+from departments.serializers import WrglFileSerializer, DepartmentOfficerSerializer
+from officers.models import Officer, Event
 from shared.serializers.es_serializers import DocumentsESSerializer
 from shared.serializers import DepartmentSerializer, DocumentWithTextContentSerializer
 from utils.es_pagination import ESPagination
@@ -51,6 +55,64 @@ class DepartmentsViewSet(viewsets.ViewSet):
             data = DocumentWithTextContentSerializer(page, many=True).data
 
         return paginator.get_paginated_response(data)
+
+    @action(detail=True, methods=['get'], url_path='officers')
+    def officers(self, request, pk):
+        department = get_object_or_404(Department, slug=pk)
+
+        starred_officers = department.starred_officers.prefetch_related(
+            Prefetch(
+                'person__officers',
+                queryset=Officer.objects.order_by('canonical_person')
+            ),
+            Prefetch(
+                'person__officers__events',
+                queryset=Event.objects.order_by(
+                    F('year').desc(nulls_last=True),
+                    F('month').desc(nulls_last=True),
+                    F('day').desc(nulls_last=True),
+                )
+            ),
+        ).annotate(
+            is_starred=Value(True, output_field=BooleanField()),
+            use_of_forces_count=Count('use_of_forces'),
+        ).all()[:DEPARTMENTS_LIMIT]
+
+        starred_officers_count = starred_officers.count()
+        sorted_featured_officers = []
+
+        if starred_officers_count < DEPARTMENTS_LIMIT:
+            featured_officers = department.officers.filter(
+                canonical_person__isnull=False,
+            ).exclude(
+                id__in=starred_officers,
+            )
+
+            sorted_featured_officers = Officer.objects.prefetch_related(
+                Prefetch(
+                    'person__officers',
+                    queryset=Officer.objects.order_by('canonical_person')
+                ),
+                Prefetch(
+                    'person__officers__events',
+                    queryset=Event.objects.order_by(
+                        F('year').desc(nulls_last=True),
+                        F('month').desc(nulls_last=True),
+                        F('day').desc(nulls_last=True),
+                    )
+                ),
+            ).filter(
+                id__in=featured_officers
+            ).annotate(
+                is_starred=Value(False, output_field=BooleanField()),
+                use_of_forces_count=Count('use_of_forces'),
+            ).order_by('-person__all_complaints_count')[:DEPARTMENTS_LIMIT - starred_officers_count]
+
+        officers = list(chain(starred_officers, sorted_featured_officers))
+
+        officers_serializers = DepartmentOfficerSerializer(officers, many=True)
+
+        return Response(officers_serializers.data)
 
     @action(detail=True, methods=['get'], url_path='datasets')
     def datasets(self, request, pk):
