143efe5d6f339b9b6f32bb2291c31d17960ca8b6
HuyN + BaoD <huy.d.nguyen+bao.dang@eastagile.com>
2022-05-09 15:29:01 +0700

    [f] Update use_of_forces model and its relations

--- ipno/use_of_forces/factories/__init__.py
+++ ipno/use_of_forces/factories/__init__.py
@@ -1,5 +1,9 @@
 from .use_of_force_factory import UseOfForceFactory
+from .use_of_force_officer_factory import UseOfForceOfficerFactory
+from .use_of_force_citizen_factory import UseOfForceCitizenFactory
 
 __all__ = [
     'UseOfForceFactory',
+    'UseOfForceOfficerFactory',
+    'UseOfForceCitizenFactory',
 ]
