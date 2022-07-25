ef9f11314152c471595498d37090d59466415078
East Agile <open-source@eastagile.com>
2022-07-25 13:47:20 +0700

    [f] 557 - Admin user can hide news articles from the front page (with confirmation)
    [#182754557](https://www.pivotaltracker.com/story/show/182754557)

--- ipno/news_articles/views.py
+++ ipno/news_articles/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page, cache_control
 
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from news_articles.models import NewsArticle
@@ -18,10 +20,22 @@ class NewsArticlesViewSet(viewsets.ViewSet):
     @cache_control(no_store=True)
     def list(self, request):
         news_articles = NewsArticle.objects.select_related('source').filter(
-            matched_sentences__officers__isnull=False
+            matched_sentences__officers__isnull=False,
+            is_hidden=False
         ).order_by(
             '-published_date',
         ).distinct()[:NEWS_ARTICLES_LIMIT]
 
         serializer = NewsArticleSerializer(news_articles, many=True)
         return Response(serializer.data)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAdminUser])
+    def hide(self, request, pk=None):
+        queryset = NewsArticle.objects.all()
+        news_article = get_object_or_404(queryset, pk=pk)
+
+        news_article.is_hidden = True
+        news_article.save()
+        return Response({
+            'detail': 'the news articles is hidden'
+        })
--- ipno/test_utils/auth_api_test_case.py
+++ ipno/test_utils/auth_api_test_case.py
@@ -7,8 +7,19 @@ from authentication.factories import UserFactory
 class AuthAPITestCase(APITestCase):
     def setUp(self):
         user = UserFactory(email='username@email.com')
-        refresh_token = RefreshToken.for_user(user)
+        admin = UserFactory(
+            email='admin@email.com',
+            is_admin=True,
+        )
+
+        user_refresh_token = RefreshToken.for_user(user)
+        admin_refresh_token = RefreshToken.for_user(admin)
 
         self.user = user
+        self.admin = admin
+
         self.auth_client = APIClient()
-        self.auth_client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(refresh_token.access_token)}')
+        self.auth_client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(user_refresh_token.access_token)}')
+
+        self.admin_client = APIClient()
+        self.admin_client.credentials(HTTP_AUTHORIZATION=f'Bearer {str(admin_refresh_token.access_token)}')
