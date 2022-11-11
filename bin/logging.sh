b4cd3501b906139351339e150ce7bdeb1048db10
HuyN + BaoD <huy.d.nguyen+bao.dang@eastagile.com>
2022-11-11 17:09:41 +0700

    [c] Remove fluentbit

--- bin/logging.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-kubectl delete -f kubernetes/logging/fluentbit-daemonset.yaml
-kubectl apply -f kubernetes/logging/fluentbit-configmap.yaml
-kubectl apply -f kubernetes/logging/fluentbit-daemonset.yaml
