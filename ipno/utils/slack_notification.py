be77922c1bdd73c8f22def766cd99f475fba003e
BaoD + HungN <bao.dang@eastagile.com+hung.d.nguyen@eastagile.com>
2023-04-04 14:36:04 +0700

    [c] Fix errors in importer

--- ipno/utils/slack_notification.py
+++ ipno/utils/slack_notification.py
@@ -6,7 +6,7 @@ from slack_sdk.errors import SlackApiError
 client = WebClient(token=settings.SLACK_BOT_TOKEN)
 
 
-def notify_slack(message):
+def notify_slack(message):  # pragma: no cover
     try:
         client.chat_postMessage(channel=settings.SLACK_CHANNEL, text=message)
     except SlackApiError as e:
