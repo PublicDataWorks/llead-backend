6683da166501b2caf67c335cd2fc18862f37d866
East Agile <open-source@eastagile.com>
2024-04-04 09:33:54 +0700

    [f] 841A - WIP - Update event importer
    https://www.pivotaltracker.com/story/show/187328846
    
    Co-authored-by: BaoD <bao.dang@eastagile.com>

--- ipno/data/tests/services/test_event_importer.py
+++ ipno/data/tests/services/test_event_importer.py
@@ -11,6 +11,7 @@ from data.constants import IMPORT_LOG_STATUS_FINISHED
 from data.factories import WrglRepoFactory
 from data.models import ImportLog
 from data.services import EventImporter
+from data.tests.services.util import MockDataReconciliation
 from departments.factories import DepartmentFactory
 from officers.factories import EventFactory, OfficerFactory
 from officers.models import Event
@@ -72,7 +73,7 @@ class EventImporterTestCase(TestCase):
         self.event5_data = [getattr(event_5, field) for field in self.header]
 
         Event.objects.all().delete()
-        self.event_importer = EventImporter()
+        self.event_importer = EventImporter("csv_file_path")
 
     def test_process_successfully(self):
         EventFactory(event_uid="event-uid1")
@@ -96,35 +97,6 @@ class EventImporterTestCase(TestCase):
         complaint_1 = ComplaintFactory(allegation_uid="complaint-uid1")
         complaint_3 = ComplaintFactory(allegation_uid="complaint-uid2")
 
-        assert Event.objects.count() == 4
-
-        hash = "3950bd17edfd805972781ef9fe2c6449"
-
-        WrglRepoFactory(
-            data_model=EventImporter.data_model,
-            repo_name="event_repo",
-            commit_hash="bf56dded0b1c4b57f425acb75d48e68c",
-            latest_commit_hash=hash,
-        )
-
-        self.event_importer.branch = "main"
-
-        mock_commit = MagicMock()
-        mock_commit.table.columns = self.header
-        mock_commit.sum = hash
-
-        self.event_importer.repo = Mock()
-        self.event_importer.new_commit = mock_commit
-
-        self.event_importer.retrieve_wrgl_data = Mock()
-
-        self.event_importer.old_column_mappings = {
-            column: self.header.index(column) for column in self.header
-        }
-        self.event_importer.column_mappings = {
-            column: self.header.index(column) for column in self.header
-        }
-
         processed_data = {
             "added_rows": [self.event4_data, self.event5_data],
             "deleted_rows": [
@@ -134,9 +106,12 @@ class EventImporterTestCase(TestCase):
                 self.event1_data,
                 self.event2_data,
             ],
+            "columns_mapping": {
+                column: self.header.index(column) for column in self.header
+            },
         }
 
-        self.event_importer.process_wrgl_data = Mock(return_value=processed_data)
+        self.event_importer.data_reconciliation = MockDataReconciliation(processed_data)
 
         result = self.event_importer.process()
 
@@ -144,7 +119,7 @@ class EventImporterTestCase(TestCase):
 
         assert import_log.data_model == EventImporter.data_model
         assert import_log.status == IMPORT_LOG_STATUS_FINISHED
-        assert import_log.commit_hash == "3950bd17edfd805972781ef9fe2c6449"
+        assert not import_log.commit_hash
         assert import_log.created_rows == 2
         assert import_log.updated_rows == 2
         assert import_log.deleted_rows == 1
@@ -154,8 +129,6 @@ class EventImporterTestCase(TestCase):
         assert result
 
         assert Event.objects.count() == 5
-
-        self.event_importer.retrieve_wrgl_data.assert_called_with("event_repo")
 
         check_columns = self.header + [
             "department_id",
@@ -236,207 +209,6 @@ class EventImporterTestCase(TestCase):
                 "raw_date",
             ]
 
-            integer_field_attrs = [
-                "year",
-                "month",
-                "day",
-            ]
-            decimal_field_attrs = [
-                "salary",
-            ]
-
-            for attr in field_attrs:
-                assert getattr(event, attr) == (
-                    event_data[check_column_mappings[attr]]
-                    if event_data[check_column_mappings[attr]]
-                    else None
-                )
-
-            for attr in integer_field_attrs:
-                assert getattr(event, attr) == (
-                    int(event_data[check_column_mappings[attr]])
-                    if event_data[check_column_mappings[attr]]
-                    else None
-                )
-
-            for attr in decimal_field_attrs:
-                assert getattr(event, attr) == (
-                    Decimal(event_data[check_column_mappings[attr]])
-                    if event_data[check_column_mappings[attr]]
-                    else None
-                )
-
-            event_complaint_ids = set(event.complaints.values_list("id", flat=True))
-
-            assert (
-                event_complaint_ids
-                == event_data[check_column_mappings["complaint_ids"]]
-            )
-
-    def test_process_successfully_with_columns_changed(self):
-        EventFactory(event_uid="event-uid1")
-        EventFactory(event_uid="event-uid2")
-        EventFactory(event_uid="event-uid3")
-        EventFactory(event_uid="event-uid6")
-
-        department_1 = DepartmentFactory(agency_name="New Orleans PD")
-        department_2 = DepartmentFactory(agency_name="Baton Rouge PD")
-
-        officer_1 = OfficerFactory(uid="officer-uid1")
-        officer_2 = OfficerFactory(uid="officer-uid2")
-        officer_3 = OfficerFactory(uid="officer-uid3")
-
-        uof_1 = UseOfForceFactory(uof_uid="uof-uid1")
-        uof_2 = UseOfForceFactory(uof_uid="uof-uid2")
-
-        appeal_1 = AppealFactory(appeal_uid="appeal-uid1")
-        appeal_2 = AppealFactory(appeal_uid="appeal-uid2")
-
-        complaint_1 = ComplaintFactory(allegation_uid="complaint-uid1")
-        complaint_3 = ComplaintFactory(allegation_uid="complaint-uid2")
-
-        assert Event.objects.count() == 4
-
-        hash = "3950bd17edfd805972781ef9fe2c6449"
-
-        WrglRepoFactory(
-            data_model=EventImporter.data_model,
-            repo_name="event_repo",
-            commit_hash="bf56dded0b1c4b57f425acb75d48e68c",
-            latest_commit_hash=hash,
-        )
-
-        self.event_importer.branch = "main"
-
-        mock_commit = MagicMock()
-        mock_commit.table.columns = self.header
-        mock_commit.sum = hash
-
-        self.event_importer.repo = Mock()
-        self.event_importer.new_commit = mock_commit
-
-        self.event_importer.retrieve_wrgl_data = Mock()
-
-        deleted_index = self.header.index("department_desc")
-        old_columns = self.header[0:deleted_index] + self.header[deleted_index + 1 :]
-        self.event_importer.old_column_mappings = {
-            column: old_columns.index(column) for column in old_columns
-        }
-        self.event_importer.column_mappings = {
-            column: self.header.index(column) for column in self.header
-        }
-
-        processed_data = {
-            "added_rows": [self.event4_data, self.event5_data],
-            "deleted_rows": [
-                self.event3_data[0:deleted_index]
-                + self.event3_data[deleted_index + 1 :],
-            ],
-            "updated_rows": [
-                self.event1_data,
-                self.event2_data,
-            ],
-        }
-
-        self.event_importer.process_wrgl_data = Mock(return_value=processed_data)
-
-        result = self.event_importer.process()
-
-        import_log = ImportLog.objects.order_by("-created_at").last()
-
-        assert import_log.data_model == EventImporter.data_model
-        assert import_log.status == IMPORT_LOG_STATUS_FINISHED
-        assert import_log.commit_hash == "3950bd17edfd805972781ef9fe2c6449"
-        assert import_log.created_rows == 2
-        assert import_log.updated_rows == 2
-        assert import_log.deleted_rows == 1
-        assert not import_log.error_message
-        assert import_log.finished_at
-
-        assert result
-
-        assert Event.objects.count() == 5
-
-        self.event_importer.retrieve_wrgl_data.assert_called_with("event_repo")
-
-        check_columns = self.header + [
-            "department_id",
-            "officer_id",
-            "use_of_force_id",
-            "complaint_ids",
-            "appeal_id",
-        ]
-        check_column_mappings = {
-            column: check_columns.index(column) for column in check_columns
-        }
-
-        expected_event1_data = self.event1_data.copy()
-        expected_event1_data.append(department_1.id)
-        expected_event1_data.append(officer_1.id)
-        expected_event1_data.append(None)
-        expected_event1_data.append({complaint_1.id})
-        expected_event1_data.append({appeal_1.id})
-
-        expected_event2_data = self.event2_data.copy()
-        expected_event2_data.append(department_1.id)
-        expected_event2_data.append(None)
-        expected_event2_data.append(uof_1.id)
-        expected_event2_data.append(set())
-        expected_event2_data.append(None)
-
-        expected_event3_data = self.event3_data.copy()
-        expected_event3_data.append(department_2.id)
-        expected_event3_data.append(officer_2.id)
-        expected_event3_data.append(uof_2.id)
-        expected_event3_data.append(set())
-        expected_event3_data.append({appeal_2.id})
-
-        expected_event4_data = self.event4_data.copy()
-        expected_event4_data.append(department_1.id)
-        expected_event4_data.append(None)
-        expected_event4_data.append(None)
-        expected_event4_data.append(set())
-        expected_event4_data.append(None)
-
-        expected_event5_data = self.event5_data.copy()
-        expected_event5_data.append(department_2.id)
-        expected_event5_data.append(officer_3.id)
-        expected_event5_data.append(None)
-        expected_event5_data.append({complaint_3.id})
-        expected_event5_data.append(None)
-
-        expected_events_data = [
-            expected_event1_data,
-            expected_event2_data,
-            expected_event4_data,
-            expected_event5_data,
-        ]
-
-        for event_data in expected_events_data:
-            event = Event.objects.filter(
-                event_uid=event_data[check_column_mappings["event_uid"]]
-            ).first()
-            assert event
-            field_attrs = [
-                "agency",
-                "left_reason",
-                "uof_uid",
-                "time",
-                "appeal_uid",
-                "kind",
-                "rank_desc",
-                "division_desc",
-                "department_desc",
-                "allegation_uid",
-                "rank_code",
-                "salary_freq",
-                "department_code",
-                "badge_no",
-                "overtime_annual_total",
-                "uid",
-                "event_uid",
-                "raw_date",
-            ]
             integer_field_attrs = [
                 "year",
                 "month",
@@ -507,12 +279,6 @@ class EventImporterTestCase(TestCase):
         department = DepartmentFactory(agency_name="New Orleans PD")
 
         EventFactory(department=department)
-        self.event_importer.old_column_mappings = {
-            column: self.header.index(column) for column in self.header
-        }
-        self.event_importer.column_mappings = {
-            column: self.header.index(column) for column in self.header
-        }
 
         event = [
             "event-uid",
@@ -551,13 +317,17 @@ class EventImporterTestCase(TestCase):
 
         self.event_importer.new_event_uids = ["event-uid"]
 
-        self.event_importer.import_data(
+        self.event_importer.data_reconciliation = MockDataReconciliation(
             {
                 "added_rows": [],
                 "updated_rows": [],
                 "deleted_rows": [event],
+                "columns_mapping": {
+                    column: self.header.index(column) for column in self.header
+                },
             }
         )
+        self.event_importer.process()
 
         assert self.event_importer.new_event_uids == ["event-uid"]
         assert self.event_importer.delete_events_ids == []
