471f08e9f443c491de4bce8c5269b9ff4907878f
Thuan Ngo <thuan.ngo@eastagile.com>
2024-04-08 15:32:42 +0700

    [c] 865 - Clean up WRGL repo
    https://www.pivotaltracker.com/story/show/187328865
    
    Co-authored-by: BaoD <bao.dang@eastagile.com>

--- ipno/data/constants.py
+++ ipno/data/constants.py
@@ -13,64 +13,6 @@ NEWS_ARTICLE_CLASSIFICATION_MODEL_NAME = "newsarticleclassification"
 PERSON_MODEL_NAME = "person"
 BRADY_MODEL_NAME = "brady"
 POST_OFFICE_HISTORY_MODEL_NAME = "postofficerhistory"
-WRGL_REPOS_DEFAULT = [
-    {
-        "repo_name": "agency-reference-list",
-        "data_model": AGENCY_MODEL_NAME,
-    },
-    {
-        "repo_name": "personnel",
-        "data_model": OFFICER_MODEL_NAME,
-    },
-    {
-        "repo_name": "event",
-        "data_model": EVENT_MODEL_NAME,
-    },
-    {
-        "repo_name": "allegation",
-        "data_model": COMPLAINT_MODEL_NAME,
-    },
-    {
-        "repo_name": "use-of-force",
-        "data_model": USE_OF_FORCE_MODEL_NAME,
-    },
-    {
-        "repo_name": "citizens",
-        "data_model": CITIZEN_MODEL_NAME,
-    },
-    {
-        "repo_name": "documents",
-        "data_model": DOCUMENT_MODEL_NAME,
-    },
-    {
-        "repo_name": "news_articles",
-        "data_model": NEWS_ARTICLE_MODEL_NAME,
-    },
-    {
-        "repo_name": "news_articles_officers",
-        "data_model": NEWS_ARTICLE_OFFICER_MODEL_NAME,
-    },
-    {
-        "repo_name": "person",
-        "data_model": PERSON_MODEL_NAME,
-    },
-    {
-        "repo_name": "appeal-hearing",
-        "data_model": APPEAL_MODEL_NAME,
-    },
-    {
-        "repo_name": "brady",
-        "data_model": BRADY_MODEL_NAME,
-    },
-    {
-        "repo_name": "news_article_classification",
-        "data_model": NEWS_ARTICLE_CLASSIFICATION_MODEL_NAME,
-    },
-    {
-        "repo_name": "post-officer-history",
-        "data_model": POST_OFFICE_HISTORY_MODEL_NAME,
-    },
-]
 
 IMPORT_LOG_STATUS_STARTED = "started"
 IMPORT_LOG_STATUS_NO_NEW_COMMIT = "no_new_commit"
